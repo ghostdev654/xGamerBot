fdb2d2aa8cafb112062b9735760ffa232607541a
ghostdev654 <ghostdev.js@yahoo.com>
2025-11-18 15:50:26 -0300

    Create autoChat.js

---
+++ autoChat.js
@@ -0,0 +1,6 @@
+𝙖𝙪𝙩𝙤𝘾𝙝𝙖𝙩.𝙟𝙨
+
+/* Acá es donde se ejecuta la lógica del Autochat
+de subbots Premium. En vez de hacerlo directamente 
+desde indexsubbots, se hace un archivo separado y
+se llama desde el evento de message.upsert */
